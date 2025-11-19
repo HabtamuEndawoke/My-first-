7fe01ea0fdf3c3180c95fc96ca4340186852c723
HabtamuEndawoke <habimyk@gmail.com>
2025-11-19 04:17:47 +0300

    It's a order

---
+++ only substraction .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    double result = num1 - num2;
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
